4b7cf8230320496caa3c505220cd8a8bd82a4bf4
lqsky7 <catinice@outlook.com>
2024-09-07 23:34:29 +0530

    implement map and map sorting logic
    Signed-off-by: lqsky7 <catinice@outlook.com>

--- round_2/question2/solution.cpp
+++ round_2/question2/solution.cpp
@@ -22,27 +22,40 @@ for(int i=0;i<buildings.size();i++){
     
 
     else{
-        buildingIndexTracker.insert(std::make_pair(i,counter)); 
+        buildingIndexTracker.insert(std::make_pair(i,counter)); //cant use counter as key because it might repeat. 
         counter = 0;
         
         }
     }
 
-
 for(const auto& z: buildingIndexTracker){           //debugging
     std::cout<<z.first<<" "<<z.second<<endl;        //debugging
 }
+//somehow sort the map by value-> create a new empty map, sort the old map by value and insert key value pair into new map
+map<int, int> BuildingIndexTracker_SORTED;
 
 
+// ????? this stuff should be looped somehow: Done.
+
+for(int i=1;i<=buildingIndexTracker.size();i++){
+    int max1 = 0;
 
 
+//find key with that max value: done
+//insert that into new map; done
 
+for( const auto& z: buildingIndexTracker){
+    max1 = max(max1, z.second);
 
+for(const auto& z: buildingIndexTracker){
+    if(max1 == z.second){
+        BuildingIndexTracker_SORTED.insert(std::make_pair(z.first,z.second));
+    }
+}
+}
+}
 
-
-
-
-
-
+// define a check function that sums from index i to i-counter+1 and returns true if sum is less than given energy
+// profit
 
 }
