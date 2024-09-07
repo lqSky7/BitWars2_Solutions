598cf433bfbe86c979aeb02116f19cbe76a4fd7b
lqsky7 <catinice@outlook.com>
2024-09-07 22:05:29 +0530

    do not debug inside a loop
    Signed-off-by: lqsky7 <catinice@outlook.com>

--- round_2/question2/solution.cpp
+++ round_2/question2/solution.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main(){
 // internal test inputs:
-vector<int> buildings{1,2,3,1,5,18,25,24,1,5,17,23};
-vector<int> energyArr{1,2,3,1,5,18,25,24,1,1,1,1};
+vector<int> buildings{1,2,3,1,5,18,25,24,1,5,23};
+vector<int> energyArr{1,2,3,1,5,18,25,24,1,1,1};
 int givenEnergy = 10;
 
 
 int counter = 0;
-map<int, int> buildingIndexTracker;
+unordered_map<int, int> buildingIndexTracker;
 
 
 
@@ -22,11 +22,27 @@ for(int i=0;i<buildings.size();i++){
     
 
     else{
-        buildingIndexTracker.insert(std::make_pair(i,counter));
-        for(const auto& z: buildingIndexTracker){           //debugging
-            std::cout<<z.first<<" "<<z.second<<endl;        //debugging
+        buildingIndexTracker.insert(std::make_pair(i,counter)); 
+        counter = 0;
+        
         }
     }
+
+
+for(const auto& z: buildingIndexTracker){           //debugging
+    std::cout<<z.first<<" "<<z.second<<endl;        //debugging
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
