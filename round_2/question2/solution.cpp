fe0a1746c39d237f6f294b8489c56755a464e282
lqsky7 <catinice@outlook.com>
2024-09-12 11:27:09 +0530

    fix some logic, but it's still incorrect

--- round_2/question2/solution.cpp
+++ round_2/question2/solution.cpp
@@ -4,34 +4,40 @@ using namespace std;
 
 int main(){
 // internal test inputs:
-vector<int> buildings{1,2,3,1,5,18,25,26,30,35,24,1,5,23,800};
-vector<int> energyArr{1,2,50,1,5,18,25,24,23,23,23,1,1,1,1};
+vector<int> buildings{1,2,3,4,1,2,3,4};
+vector<int> energyArr{5,5,5,5,5,5,5,5};
+
 int givenEnergy = 11;
 int temp = givenEnergy;
 
 int max1 =1;
 int cnt = 0;
-
+//issue 1: energy should be reset only when a valid building sequence starts?? is it really an issue? why not just reset when it's zero and record the value in max1?
+// issue 2
 for(int i=0;i<buildings.size()-1;i++)
 {
     if(buildings[i]<buildings[i+1])
-    {
+    {   
+        
         if(givenEnergy>0){
         givenEnergy -= energyArr[i];
         cnt++;
+        // cout<<"if 1 debug:"<<endl<<cnt<<endl<<max1<<endl;
         }
     }
-    else if(buildings[i] >buildings[i+1])
+    if(buildings[i] >buildings[i+1])
     {   max1 = max(max1, cnt);
         cnt = 0;
-        givenEnergy = temp;
+        // cout<<"if 2 debug:"<<endl<<givenEnergy<<endl<<cnt<<endl<<max1<<endl;
     }
-    else if(givenEnergy < 0){
+    if(givenEnergy < 0){
         max1 = max(max1, cnt);
+        // cout<<"if 3 debug:"<<endl<<cnt<<endl<<max1<<endl;
         givenEnergy = temp;
+        cnt =0;
 
     }
 
 }
-        cout<<max1<<endl;
+        cout<<endl<<max1<<endl;
 };
