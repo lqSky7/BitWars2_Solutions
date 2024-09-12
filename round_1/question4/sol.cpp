0a73b064a1bd172fada113e4349e4b7c284aca31
lqsky7 <catinice@outlook.com>
2024-09-12 22:21:16 +0530

    update logic

--- round_1/question4/sol.cpp
+++ round_1/question4/sol.cpp
@@ -1,32 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-vector<int> flwrArr = {0,0,0,0,1,0,0,0};
-// int numberofEmptyPlots = flwrArr.size() -accumulate(flwrArr.begin(), flwrArr.end(), 0);
+void nowprinter(vector<int> a){
+    for(const auto i: a){cout<<i<<endl;}
 
-// int numberFlw = 50;
+};
+
+int main(){
+vector<int> flwrArr = {1,0,0,1,1,0,0,0,0,1,1,1,1};  //fix this test case pls
+
+int numberFlw = 1;
 
 bool safeArr[flwrArr.size()] = {};
 for(int i=0;i<flwrArr.size();i++){
     safeArr[i] = true;
 }
 
-// for(const auto r: safeArr){
-//     cout<<r<<endl;
-// }
+//edge cases
+if(flwrArr[0]==1){ 
+    safeArr[0] = false;
+}
+
+if(flwrArr[-1]==1){
+    safeArr[0] = false;
+}
+
+
 
 for(int i = 1;i<flwrArr.size()-1;i++){
-    if(flwrArr[i]==1){safeArr[i-1]=safeArr[i]=safeArr[i+1]=false;}
+    if(flwrArr[i]==1){safeArr[i-1]=safeArr[i]=safeArr[i+1]=false;
+    
+    }
+
+
+}
+for(int i = 1;i<flwrArr.size()-1;i++){
+    if(safeArr[i]==safeArr[i+1]==true){
+        safeArr[i] = false;
+    }
 }
 
 int cnt =0;
 for(int i=0;i<flwrArr.size();i++){
     if(safeArr[i] == true){
-        cnt++;
+      cnt++;
         
     }
 }
-cout<<cnt<<endl;
-
+if(cnt>=numberFlw){
+    cout<<"Safe!"<<endl;
+    cout<<endl<<cnt<<endl;
+}
+else{cout<<"Not safe"<<endl;}
+cout<<endl<<cnt<<endl;
 };
